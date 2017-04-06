a0f2c94eb2b29b6b43f74da2cd22ea0bbc5df102
HenriHuh <henrihuhtala@kamk.fi>
2017-04-06 10:14:01 +0300

    movement

---
+++ 2.5D Side Scorller/Assets/New Scripts/Controller.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent(typeof(PlayerPhysics))]
+public class Controller : MonoBehaviour {
+
+    public float gravity;
+    public float speed;
+    public float acceleration;
+    public float jumpHeight;
+
+    private Animator anim;
+
+    private float curSpeed;
+    private float tarSpeed;
+    private Vector2 amountToMove;
+
+    PlayerPhysics playerPhysics;
+
+    void Start()
+    {
+        playerPhysics = GetComponent<PlayerPhysics>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+
+        if (playerPhysics.Grounded || playerPhysics.sloped)
+        {
+            amountToMove.y = 0;
+            if (Input.GetKeyDown(KeyCode.X))
+            {
+                amountToMove.y = jumpHeight;
+            }
+        }
+
+        if (playerPhysics.FacingWall)
+        {
+            tarSpeed = 0;
+            curSpeed = 0;
+        }
+
+        tarSpeed = Input.GetAxis("Horizontal") * speed;
+        curSpeed = IncrementTowards(curSpeed, tarSpeed, acceleration);
+
+        //Animator things
+        anim.SetFloat("Speed", Mathf.Abs(tarSpeed));
+        //---------------
+
+        amountToMove.x = curSpeed;
+
+        if (playerPhysics.Roofed)
+        {
+            amountToMove.y = 0;
+        }
+
+        if (!playerPhysics.Grounded || !playerPhysics.sloped) //!!!!!!!!!!!?????????
+        {
+            amountToMove.y -= gravity * Time.deltaTime;
+        }
+        playerPhysics.Move(amountToMove * Time.deltaTime);
+    }
+
+    private float IncrementTowards(float n, float target, float a)
+    {
+        if (n == target)
+        {
+            return n;
+        }
+        else
+        {
+            float dir = Mathf.Sign(target - n);
+            n += a * Time.deltaTime * dir;
+            return (dir == Mathf.Sign(target - n)) ? n : target;
+        }
+    }
+}
---
+++ 2.5D Side Scorller/Assets/New Scripts/PlayerPhysics.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerPhysics : MonoBehaviour {
+
+    private BoxCollider collider;
+    private Vector3 s;
+    private Vector3 c;
+
+    public float deltaY;
+    public float deltaX;
+
+    public LayerMask colMask;
+    public LayerMask slopeMask;
+
+    public bool Grounded;
+    public bool Roofed = false;
+    public bool FacingWall = false;
+    public bool sloped = false;
+
+    public float angle; //SLOPE
+
+    Ray ray;
+
+
+    void Start()
+    {
+        collider = GetComponent<BoxCollider>();
+        s = collider.size;
+        c = collider.center;
+    }
+
+    public void Move(Vector2 moveAmount)
+    {
+
+        Grounded = false;
+        FacingWall = false;
+        Roofed = false;
+        sloped = false;
+
+        //SLOPES ARE CALCULATED HERE
+        RaycastHit hit1;
+        RaycastHit hit2;
+        if (Physics.Raycast(transform.position, Vector2.down, out hit1) && Physics.Raycast(transform.position +  Vector3.right * 0.1F, Vector2.down, out hit2))
+        {
+            angle = Mathf.Atan2(hit1.normal.x, hit2.normal.y) * Mathf.Rad2Deg;
+        }
+
+        //MOVEMENT
+        deltaY = moveAmount.y;
+        deltaX = moveAmount.x;
+        Vector2 p = transform.position;
+        float dirX = Mathf.Sign(moveAmount.x);
+        float dirY = Mathf.Sign(moveAmount.y);
+
+
+        //RAYCASTING FOR OBSTACLES
+
+        if (Physics.Raycast(transform.position, Vector2.down, -dirY * 0.15F, colMask) || Physics.Raycast(transform.position + transform.right / 2, Vector2.down, -dirY * 0.15F, colMask) || Physics.Raycast(transform.position - transform.right / 2, Vector2.down, -dirY * 0.15F, colMask))
+        {
+            Grounded = true;
+            deltaY = 0;
+        }
+        else if (Physics.Raycast(transform.position, Vector2.down, -dirY * 0.15F, slopeMask) || Physics.Raycast(transform.position + transform.right / 2, Vector2.down, -dirY * 0.15F, slopeMask) || Physics.Raycast(transform.position - transform.right / 2, Vector2.down, -dirY * 0.15F, slopeMask))
+        {
+            sloped = true;
+            deltaY = 0;
+        }
+        else if (Physics.Raycast(transform.position, Vector2.up, dirY * 1.2F, colMask) || Physics.Raycast(transform.position + transform.right / 2, Vector2.up, dirY * 1.2F, colMask) || Physics.Raycast(transform.position - transform.right / 2, Vector2.up, dirY * 1.2F, colMask))
+        {
+            Roofed = true;
+            deltaY = 0;
+        }
+
+        if (Physics.Raycast(transform.position, Vector2.right, dirX / 2, colMask) || Physics.Raycast(transform.position + transform.up, Vector2.right, dirX / 2, colMask) || Physics.Raycast(transform.position - transform.up * 0.001F, Vector2.right, dirX / 2, colMask))
+        {
+            FacingWall = true;
+            deltaX = 0;
+        }
+        else if (Physics.Raycast(transform.position, Vector2.left, -dirX / 2, colMask) || Physics.Raycast(transform.position + transform.up, Vector2.left, -dirX / 2, colMask) || Physics.Raycast(transform.position - transform.up * 0.001F, Vector2.left, -dirX / 2, colMask))
+        {
+            FacingWall = true;
+            deltaX = 0;
+        }
+        
+        if ((deltaX > 0 || deltaX < 0) && sloped)
+        {
+            float angleDir = Mathf.Sign(angle);
+            deltaY = 0.5F * -angleDir * moveAmount.x;
+        }
+        
+
+
+        Vector2 finalTransform = new Vector2(deltaX, deltaY);
+        transform.Translate(finalTransform);
+    }
+}
